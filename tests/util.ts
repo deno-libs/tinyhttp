8c8c32a213a891cd38bfac0c06f14c9c14920a5b
v1rtl <pilll.pl22@gmail.com>
2021-03-07 18:00:42 +0300

    remove test utils

--- tests/util.ts
+++ tests/util.ts
@@ -10,49 +10,6 @@ export const randomPort = async () => {
   return await getFreePort(random(2048, 8064))
 }
 
-export function describe(_name: string, fn: () => void | Promise<void>) {
-  fn()
-}
-
-export const TEST_TIMEOUT = 3000
-
-export function it(name: string, fn: (done?: unknown) => void | Promise<void>) {
-  Deno.test(name, async () => {
-    let done = (err?: unknown) => {
-      if (err) throw err
-    }
-    let race: Promise<unknown> = Promise.resolve()
-
-    if (fn.length === 1) {
-      let resolve: (value?: unknown) => void
-      const donePromise = new Promise((r) => {
-        resolve = r
-      })
-
-      let timeoutId: number
-
-      race = Promise.race([
-        new Promise(
-          (_, reject) =>
-            (timeoutId = setTimeout(() => {
-              reject(new Error(`test "${name}" failed to complete by calling "done" within ${TEST_TIMEOUT}ms.`))
-            }, TEST_TIMEOUT))
-        ),
-        donePromise
-      ])
-
-      done = (err?: unknown) => {
-        clearTimeout(timeoutId)
-        resolve()
-        if (err) throw err
-      }
-    }
-
-    await fn(done)
-    await race
-  })
-}
-
 export const BindToSuperDeno = <Req extends Request, Res extends Response>(app: App<unknown, Req, Res>) => {
   const fetch = superdeno(app.handler.bind(app))
 
