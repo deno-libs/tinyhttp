f93578484e264106208f88bdccc578c03f56d283
v1rtl <yo@v1rtl.site>
2023-05-03 15:08:50 +0300

    bump proxy_addr and content disposition

--- deps.ts
+++ deps.ts
@@ -11,14 +11,14 @@ export {
   all,
   compile,
   proxyaddr,
-} from 'https://deno.land/x/proxy_addr@0.1.2/mod.ts'
+} from 'https://deno.land/x/proxy_addr@0.1.3/mod.ts'
 export { status } from 'https://deno.land/x/status@0.1.0/status.ts'
 export { isIP } from 'https://deno.land/x/isIP@1.0.0/mod.ts'
 export { escapeHtml } from 'https://deno.land/x/escape_html@1.0.0/mod.ts'
 export * as base64 from 'https://deno.land/std@0.185.0/encoding/base64.ts'
 export { parseMediaType } from 'https://deno.land/std@0.185.0/media_types/parse_media_type.ts'
 export * as mediaTyper from 'https://deno.land/x/media_typer@1.0.1/mod.ts'
-export { contentDisposition } from 'https://esm.sh/@tinyhttp/content-disposition@2.0.8'
+export { contentDisposition } from 'https://esm.sh/@tinyhttp/content-disposition@2.0.9'
 export { extname } from 'https://deno.land/std@0.185.0/path/mod.ts'
 export {
   accepts,
